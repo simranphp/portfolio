dbd1bc1899c1735d6189e3885a39f914f4d3fe56
simran <129687241+simranphp@users.noreply.github.com>
2025-09-17 15:01:20 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,14 @@
+const siteData = {
+name: 'Luke Coleman',
+role: 'Software Developer',
+tagline: 'I excel at crafting elegant digital experiences and I am proficient in various programming languages and technologies.',
+stats: [
+{ num: 12, label: 'Years of experience' },
+{ num: 26, label: 'Projects completed' },
+{ num: 8, label: 'Technologies mastered' },
+{ num: 499, label: 'Code commits' },
+],
+}
+
+
+export default siteData;
