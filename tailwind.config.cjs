17cdc06dddf171c6e231ba50d5697afae3462a4f
simran <129687241+simranphp@users.noreply.github.com>
2025-09-17 15:02:52 +0530

    Add files via upload

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,19 +1,19 @@
 module.exports = {
-content: [
-'./pages/**/*.{js,jsx}',
-'./components/**/*.{js,jsx}',
-],
-theme: {
-extend: {
-colors: {
-neon: '#00F5A0',
-'bg-dark': '#0b0c0f',
-},
-fontFamily: {
-mono: ['"Space Mono"', 'monospace'],
-pop: ['Poppins', 'sans-serif'],
-},
-},
-},
-plugins: [],
+  content: [
+    './pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        neon: '#00F5A0',
+        'bg-dark': '#0b0c0f',
+      },
+      fontFamily: {
+        mono: ['"Space Mono"', 'monospace'],
+        pop: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
 }
